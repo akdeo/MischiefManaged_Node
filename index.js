3e9428db57e87fbf00b14ee1f629d11c95d25cd7
akdeo <akdeo@ucsc.edu>
2017-04-22 19:12:19 -0700

    fixed locations

--- index.js
+++ index.js
@@ -11,41 +11,45 @@ var users = {};
 
 //Endpoints 
 app.post('/init', function (req, res) {
-	var name = req.body.name;
-	var id = Math.floor((Math.random() * 10000) + 1)
-	// var lat = 
-	// var lon =
-	var last_update = d.getTime();
-	var user = {
-		name: name,
-		id: id,
-		last_update: last_update,
-	};
-	users[id] = user; 
-	res.status(200).json(id);
-	// res.end(id)
+    var name = req.query.name;
+    console.log(req.query.name);
+    var id = Math.floor((Math.random() * 10000) + 1)
+    // var lat = 
+    // var lon =
+    var last_update = d.getTime();
+    var user = {
+        name: name,
+        id: id,
+        last_update: last_update,
+    };
+    users[id] = user; 
+    res.status(200).json(id);
+    res.end();
 })
 
 app.put('/location', function (req, res) {
-	var lat = req.body.lat;
-	var lon = req.body.lon;
-	var id = req.body.id;
-	var user = users[id];
-	user.lat = lat;
-	user.lon = lon;
+    var lat = req.query.lat;
+    var lon = req.query.lon;
+    var id = req.query.id;
+    var user = users[id];
+    user.lat = lat;
+    user.lon = lon;
+    res.status(200);
+    res.end();
 })
 
 app.get('/map/z/x/y', function (req, res) {
-
+    res.end();
 })
 
 app.get('/locations', function (req, res) {
-	res.status(200).json(users);
-
+    res.status(200).json(users);
+    res.end();
 })
 
 app.get('/test', function(req, res) {
-	console.log("TEST");
+    console.log("TEST");
+    res.end();
 })
 
 
@@ -53,9 +57,9 @@ app.get('/test', function(req, res) {
 
 var server = app.listen(8081, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Example app listening at http://%s:%s", host, port)
 
 })
