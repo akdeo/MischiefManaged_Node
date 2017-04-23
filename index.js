f80e7de7deedd7c7a4359482ba4fce44eda3cd6a
akdeo <akdeo@ucsc.edu>
2017-04-22 18:37:22 -0700

    updated index.js '

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var d = new Date();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
@@ -11,11 +12,27 @@ var users = {};
 //Endpoints 
 app.post('/init', function (req, res) {
 	var name = req.body.name;
-
+	var id = Math.floor((Math.random() * 10000) + 1)
+	// var lat = 
+	// var lon =
+	var last_update = d.getTime();
+	var user = {
+		name: name,
+		id: id,
+		last_update: last_update,
+	};
+	users[id] = user; 
+	res.status(200).json(id);
+	// res.end(id)
 })
 
 app.put('/location', function (req, res) {
-
+	var lat = req.body.lat;
+	var lon = req.body.lon;
+	var id = req.body.id;
+	var user = users[id];
+	user.lat = lat;
+	user.lon = lon;
 })
 
 app.get('/map/z/x/y', function (req, res) {
@@ -23,9 +40,12 @@ app.get('/map/z/x/y', function (req, res) {
 })
 
 app.get('/locations', function (req, res) {
+	res.status(200).json(users);
 
+})
 
-
+app.get('/test', function(req, res) {
+	console.log("TEST");
 })
 
 
