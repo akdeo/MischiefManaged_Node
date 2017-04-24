27c5f11263ec0dc81daf8a8dca7b92417a386559
Drake Levy <drakelevy1@gmail.com>
2017-04-23 22:02:30 -0700

    Tweaks + custom map

--- index.js
+++ index.js
@@ -7,47 +7,81 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var users = {};
+var users = {
+    1: {
+        name: 'Albus Dumbledore',
+        id: 1,
+        lat: 37.872217,
+        lon: -122.257898,
+    },
+    2: {
+        name: 'Madam Hooch',
+        id: 2,
+        lat: 37.870803,
+        lon: -122.250731,
+    }
+};
 
 //Endpoints 
 app.post('/init', function (req, res) {
-    var name = req.query.name;
-    console.log(req.query.name);
-    var id = Math.floor((Math.random() * 10000) + 1)
-    // var lat = 
-    // var lon =
+    var name = req.body.name;
+    console.log(req.body);
+    var id = Math.floor((Math.random() * 10000) + 1);
+    if (req.body.id && users[req.body.id]) {
+        console.log("User with id " + req.body.id + " already exists");
+        return res.status(200).json({ id: req.body.id });
+    } else if (req.body.id) {
+        id = req.body.id;
+    }
     var last_update = d.getTime();
-    var messages = [];
     var user = {
         name: name,
         id: id,
         last_update: last_update,
-        messages: messages,
+        messages: [],
     };
-    users[id] = user; 
-    res.status(200).json(id);
-    res.end();
-})
+    users[id] = user;
+    console.log("User " + req.body.name + " given id: " + id);
+    res.status(200).json({ id });
+});
 
 app.put('/location', function (req, res) {
-    var lat = req.query.lat;
-    var lon = req.query.lon;
-    var id = req.query.id;
+    var lat = req.body.lat;
+    var lon = req.body.lon;
+    var id = req.body.id;
     var user = users[id];
     user.lat = lat;
     user.lon = lon;
     res.status(200);
     res.end();
-})
+});
 
-app.get('/map/z/x/y', function (req, res) {
-    res.end();
-})
+
+var mapLocs = new Set();
+mapLocs.add(JSON.stringify(["15", "5255", "12653"]));
+mapLocs.add(JSON.stringify(["15", "5255", "12654"]));
+mapLocs.add(JSON.stringify(["15", "5256", "12653"]));
+mapLocs.add(JSON.stringify(["15", "5256", "12654"]));
+
+app.get('/map/:z/:x/:y.png', function (req, res) {
+    z = req.params.z;
+    x = req.params.x;
+    y = req.params.y;
+    var img;
+    if (mapLocs.has(JSON.stringify([z, x, y]))) {
+        imageName = "new-" + z + "-" + x + "-" + y + ".png";
+        img = fs.readFileSync('./Map/' + imageName);
+    } else {
+        img = fs.readFileSync('./Map/blank.png');
+    }
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    res.end(img, 'binary');
+});
 
 app.get('/locations', function (req, res) {
     res.status(200).json(users);
     res.end();
-})
+});
 
 app.post('/message', function (req, res) {
     var from_id = req.query.from_id;
@@ -62,10 +96,10 @@ app.post('/message', function (req, res) {
     	message: msg,
     };
     messages.push(message);
-    user[messages] = messages;
+    user.messages = messages;
     res.status(200);
     res.end();
-})
+});
 
 app.get('/messages', function (req, res) {
     var id = req.query.id;
@@ -73,19 +107,16 @@ app.get('/messages', function (req, res) {
     var messages = user.messages;
     user.messages = [];
     res.status(200).json(messages);
-    res.end();
-})
+});
 
 app.get('/test', function(req, res) {
     console.log("TEST");
     res.end();
-})
+});
 
 var server = app.listen(8080, function () {
-
     var host = server.address().address
     var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", host, port)
-
-})
+    console.log("Mischief Managed listening at http://%s:%s", host, port)
+});
